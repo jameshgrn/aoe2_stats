1441407fb420640821d9a74d64fcb5c88e56636a
Jake Gearon <jake.gearon@gmail.com>
2024-05-12 20:06:44 -0400

    Update user authentication logic

---
+++ aoe2_stats/aoe.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH -J aoe2_data_processing
+#SBATCH -p general
+#SBATCH -o aoe2_data_processing_%j.txt
+#SBATCH -e aoe2_data_processing_%j.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jhgearon@iu.edu
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=01-02:00:00
+#SBATCH --mem=150G
+
+cd aoe2_stats/aoe2_stats
+#Load any modules that your program needs
+module load python
+
+pip -u install duckdb pandas 
+#Run your program
+python aoe2_stats/eda.py
